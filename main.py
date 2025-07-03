2864e9010b7d75d9893fdc28da436aabcf9706dd
amjad awad <amjadawad129@gmail.com>
2025-07-03 22:20:17 +0300

    update the main

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from parser import PDFExtractor,TextExtractor
 from processor import Preprocessor
 from skill import SkillListMatcher
+from similarity import SentenceTransformerSimilarity, BertSimilarity
 
 pdf_extractor = PDFExtractor()
 text_extractor = TextExtractor()
 preprocessor = Preprocessor()
 skill_matcher = SkillListMatcher()
+sentence_transformer = SentenceTransformerSimilarity()
+bert_similarity = BertSimilarity("slone/bert-base-multilingual-cased-bak-rus-similarity")
 
 resume = pdf_extractor.extract("sample_data/amjadCVAI.pdf")
 job_description = text_extractor.extract("sample_data/job_description")
@@ -31,3 +34,13 @@ print(job_description)
 print("----------------------")
 
 print(skill_matcher.match(skills, match_skills))
+print("----------------------")
+
+resume_embedding = sentence_transformer.encode(resume)
+job_description_embedding = sentence_transformer.encode(job_description)
+
+bert_resume_embedding = bert_similarity.encode(resume)
+bert_job_description_embedding = bert_similarity.encode(job_description)
+
+print(sentence_transformer.similarity(resume_embedding, job_description_embedding))
+print(bert_similarity.similarity(bert_resume_embedding, bert_job_description_embedding))
