44e8bf2a7c414ba7bf997d013b5f9f13de6bcef5
amjad awad <amjadawad129@gmail.com>
2025-07-03 22:20:05 +0300

    add doc string for the classes

--- similarity/similarity.py
+++ similarity/similarity.py
@@ -4,25 +4,55 @@ from transformers import BertTokenizerFast, BertModel
 import torch.nn.functional as F
 
 class SentenceTransformerSimilarity:
+    """
+    A class for computing sentence similarity using a SentenceTransformer model.
+    """
     def __init__(self, model_name="all-MiniLM-L6-v2"):
+        """
+        Initializes the SentenceTransformerSimilarity with a given model.
+        :param model_name: The name of the pretrained SentenceTransformer model
+        """
         self.model = SentenceTransformer(model_name)
 
 
     def encode(self, sentence):
+        """
+        Encodes a sentence into a dense vector representation.
+        :param sentence: Input sentence to encode.
+        :return: Embedding vector of the sentence.
+        """
         return self.model.encode(sentence)
 
 
     def similarity(self, embedding1, embedding2):
+        """
+        Calculates cosine similarity between two embeddings.
+        :param embedding1: First sentence embedding.
+        :param embedding2: Second sentence embedding.
+        :return: Cosine similarity score between the two embeddings.
+        """
         return self.model.similarity(embedding1, embedding2).item()
 
 
 class BertSimilarity:
+    """
+    A class for computing sentence similarity using a pretrained BERT model.
+    """
     def __init__(self, model_name="google-bert/bert-base-uncased"):
+        """
+        Initializes the BertSimilarity with a given BERT model.
+        :param model_name: The name of the pretrained BERT model.
+        """
         self.model = BertModel.from_pretrained(model_name)
         self.tokenizer = BertTokenizerFast.from_pretrained(model_name)
 
 
     def encode(self, sentence):
+        """
+        Encodes a sentence into a dense vector using the average of the last 4 hidden layers.
+        :param sentence: Input sentence to encode.
+        :return: Sentence embedding tensor.
+        """
         inputs = self.tokenizer(sentence, return_tensors="pt", padding=True, truncation=True)
 
         with torch.no_grad():
@@ -38,4 +68,10 @@ class BertSimilarity:
 
 
     def similarity(self, embedding1, embedding2):
+        """
+        Calculates cosine similarity between two embeddings.
+        :param embedding1: First sentence embedding.
+        :param embedding2: Second sentence embedding.
+        :return: Cosine similarity score between the two embeddings.
+        """
         return F.cosine_similarity(embedding1, embedding2).item()
